246716a7a1c37fb592b7ab72511359910dbc50cd
manojreddy <manojmulukuri@outlook.com>
2025-03-13 23:06:12 +0530

    adding content

---
+++ variables.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+PERSON1=RAM 
+PERSON2=SITA
+
+echo "$PERSON1: how are you, $PERSON2"
+echo "$PERSON2: im doing good, how about you, $PERSON1"
+echo "$PERSON1: im fine"
+echo "hello world"
