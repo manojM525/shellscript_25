3863ef7c611ba30f88f1509193d5c3a79329110d
manojreddy <manojmulukuri@outlook.com>
2025-03-14 16:45:35 +0530

    exploring variables and datatypes

--- arrays.sh
+++ arrays.sh
@@ -2,7 +2,7 @@
 FRUITS=("apples" "bananas" "oranges")
 
 echo "first value: ${FRUITS[0]} "
-echo "second value: ${FRUIS[1]} "
+echo "second value: ${FRUITS[1]}" 
 echo " third value: ${FRUITS[2]} "
 
 echo " all values given: ${FRUITS[@]} "
