25a520842f42e3e88f0a3f357e5ac5876ffa1965
manojreddy <manojmulukuri@outlook.com>
2025-03-18 18:54:44 +0530

    checking variables input with blankspaces

--- testing-inputswspaces.sh
+++ testing-inputswspaces.sh
@@ -1,4 +1,5 @@
 VARIABLE1="HELLO GOOD MORNING"
+VARIABLE2=HELLO GOOD MORNING
 
 echo "$VARIABLE1"
 echo "...  $VARIABLE1... "
@@ -11,4 +12,14 @@ done
 for out in "$VARIABLE1" ;
 do 
 echo " $out "
+done
+
+for rest in $VARIABLE2 ;
+do 
+echo "$rest"
+done 
+
+for focus in "$VARIABLE2" ;
+do 
+echo " $focus "
 done
